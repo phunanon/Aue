7f0c63fa9601804aeb3b34f4a60a2774d0ae0da1
Patrick <phunanon@users.noreply.github.com>
2020-10-04 16:07:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const e = el => document.querySelector(el);
+const es = el => Array.from(document.querySelectorAll(el));
+
+let db = null;
+
+function DOM_verseHover () {
+  const cite = db.aue[this.dataset.num][0];
+  es("verse").forEach(e => e.style.opacity = 0.25);
+  this.style.opacity = 1;
+  es("interp").forEach((e, i) => {
+    const included = db.interpretations[i][2].includes(cite);
+    e.style.maxHeight = included ? "10rem" : 0;
+    e.style.opacity = included ? 1 : 0;
+  });
+}
+function DOM_interpHover () {
+  const cites = db.interpretations[this.dataset.num][2];
+  es("interp").forEach(e => e.style.opacity = 0.25);
+  this.style.opacity = 1;
+  es("verse").forEach((e, i) => e.style.opacity = cites.includes(db.aue[i][0]) ? 1 : 0.25);
+}
+function DOM_mouseLeave () {
+  es("verse").forEach(e => e.style.opacity = 1);
+  es("interp").forEach(e => e.style.opacity = 1);
+  es("interp").forEach(e => e.style.maxHeight = "10rem");
+}
+
+function htmlAueVerse ([cite, body], i) {
+  return `<verse data-num="${i}"><sup>${cite} </sup>${body}</verse>`;
+}
+
+function htmlInterp ([title, body, cites], i) {
+  return `<interp data-num="${i}"><i>${title}</i>. ${body} <sup>${cites}</sup></interp>`;
+}
+
+function DOM_display_Aue () {
+  const {aue, interpretations} = db;
+  e("aue").innerHTML = aue.map(htmlAueVerse).join("");
+  e("interps").innerHTML = interpretations.map(htmlInterp).join("");
+
+  es("verse").forEach(l => l.addEventListener('mouseover', DOM_verseHover));
+  es("interp").forEach(l => l.addEventListener('mouseover', DOM_interpHover));
+  es("verse").concat(es("interp")).forEach(l => l.addEventListener('mouseout', DOM_mouseLeave));
+}
+
+async function fetchAue () {
+  let aue = await fetch("https://raw.githubusercontent.com/phunanon/Aue/main/aue.json");
+  if (aue.ok) {
+    db = await aue.json();
+    DOM_display_Aue();
+  } else
+    alert("HTTP-Error: " + response.status);
+}
+
+function DOM_onload () {
+  fetchAue();
+}
