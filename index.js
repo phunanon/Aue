80f102a299e1b88288365fb67cfbb594e1ff72a6
Patrick.Bowen <patrick.bowen@inboxinsight.com>
2020-11-23 14:27:33 +0000

    Lean more on CSS for animations

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const e = el => document.querySelector(el);
 const es = el => Array.from(document.querySelectorAll(el));
-const ess = el => es(el).map(e => e.style);
 
 let inClick = false;
 
 function DOM_verseHover () {
   if (inClick) return;
   DOM_reset();
-  const cite = db.aue[this.dataset.num][0];
-  ess("verse").forEach(s => s.opacity = 0.25);
-  this.style.opacity = 1;
-  let numInclusions = 0;
-  ess("interp").forEach((s, i) => {
-    const included = db.interpretations[i][2].includes(cite);
-    s.maxHeight = included ? "12rem" : 0;
-    s.userSelect = included ? "" : "none";
-    numInclusions += included;
-  });
-  e("h2").innerHTML = `Interpretations (${numInclusions})`;
+  es("verse").forEach(el => el != this && el.classList.add("dim"));
+  const toHide = es("interp").filter((el, i) => !db.interpretations[i][2].includes(this.dataset.cite));
+  toHide.forEach(el => el.classList.add("hide"));
+  e("h2").innerHTML = `Interpretations (${db.interpretations.length - toHide.length})`;
 }
 function DOM_interpHover () {
   if (inClick) return;
   DOM_reset();
-  const cites = db.interpretations[this.dataset.num][2];
-  ess("interp").forEach(s => s.opacity = 0.25);
-  this.style.opacity = 1;
-  ess("verse").forEach((s, i) => s.opacity = cites.includes(db.aue[i][0]) ? 1 : 0.25);
+  es("interp").forEach(el => el != this && el.classList.add("dim"));
+  es("verse").forEach((el, i) => !this.dataset.cites.includes(db.aue[i][0]) && el.classList.add("dim"));
 }
 function DOM_click (e) {
   if (!(inClick = !inClick))
@@ -37,26 +27,17 @@ function DOM_click (e) {
 function DOM_reset () {
   if (inClick) return;
   e("h2").innerHTML = `Interpretations (${db.interpretations.length})`;
-  ess("verse").concat(ess("interp")).forEach(s => {
-    s.opacity = 1;
-    s.textDecorationColor = "transparent";
-    s.userSelect = "";
+  es("verse").concat(es("interp")).forEach(el => {
+    el.classList.remove('dim', 'hide');
+    el.style.textDecorationColor = "transparent";
   });
-  ess("interp").forEach(s => s.opacity = 1);
-  ess("interp").forEach(s => s.maxHeight = "12rem");
-}
-
-function htmlAueVerse ([cite, body], i) {
-  return `<verse data-num="${i}"><cite>${cite} </cite>${body}</verse>`;
-}
-
-function htmlInterp ([title, body, cites], i) {
-  return `<interp data-num="${i}"><i>${title}</i>. ${body} <cite>${cites}</cite></interp>`;
 }
 
 function DOM_display_Aue () {
-  e("aue").innerHTML = db.aue.map(htmlAueVerse).join("");
-  e("interps").innerHTML = db.interpretations.map(htmlInterp).join("");
+  e("aue").innerHTML = db.aue.map(([cite, body]) =>
+    `<verse data-cite="${cite}"><cite>${cite} </cite>${body}</verse>`).join("");
+  e("interps").innerHTML = db.interpretations.map(([title, body, cites]) =>
+    `<interp data-cites="${cites}"><i>${title}</i>. ${body} <cite>${cites}</cite></interp>`).join("");
 
   document.body.addEventListener('click', () => {
     if (window.getSelection().type == "Range")
